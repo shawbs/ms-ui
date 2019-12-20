3bbb0a4f18adee36020ee717a6766f3723b45ca5
shawbs <xxw.show@qq.com>
2019-12-20 17:46:17 +0800

    update

--- main/packages/pull-refresh/main.vue
+++ main/packages/pull-refresh/main.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="ms-pull-refresh">
-        <div class="ms-pull-refresh-box" :style="translate">
-            <div class="ms-pull-refresh-head" ref="pullRefreshHead">
+        <div class="ms-pull-refresh-box" ref="pullRefresh" :style="{
+            'margin-top': top ? `${-refreshHeight}px` : 0,
+            'margin-bottom': bottom ? `${-refreshHeight}px` : 0,
+            'transform': ` translate3D(0, ${y}px, 0)`,
+            'transition': doAn ? 'transform .6s ease' : 'none 0s ease'
+            }">
+            <div class="ms-pull-refresh-head" ref="pullRefreshHead" :style="{height: refreshHeight + 'px'}">
                 <ms-icon icon="loading"></ms-icon>
                 {{tip}}
             </div>
-            <div class="ms-pull-refresh-content" ref="pullRefresh">
+            <!-- <div class="ms-pull-refresh-content" ref="pullRefresh">
                 <slot />
-            </div>
+            </div> -->
+            <slot />
         </div>
     </div>
 </template>
@@ -16,11 +22,21 @@
 import {on, off} from '../../utils/index'
 export default {
     name: 'MsPullRefresh',
+    props: {
+        top: {
+            type: Boolean,
+            default: true
+        },
+        bottom: Boolean,
+        refreshHeight: {
+            type: Number,
+            default: 80
+        }
+    },
     data(){
         return {
             loading: false,
-            headH: 0,
-            top: 0,
+            y: 0,
             doAn: false,
             status: 0 //0初始 1下拉 2刷新 3刷新完成
         }
@@ -38,43 +54,37 @@ export default {
                 return '刷新完成'
             default: return ''
             }
-        },
-        translate(){
-            return `transform: translate3D(0, ${this.top}px, 0);transition: ${this.doAn ? 'transform .6s ease' : 'none 0s ease'};`
         }
     },
     mounted(){
-        this.init()
         this.slide = this.slideFunc(document.body)
     },
     beforeDestroy(){
         this.slide.destroy()
     },
     methods: {
-        init(){
-            this.headH = this.$refs.pullRefreshHead.offsetHeight
-        },
         slideFunc(container){
-            let startX, startY,moveX;
+            let startX, startY;
             let self = this
             let speed = 3 //滑动速度
             let margin = 10 //超出距离
             let num = 0 //滑动距离
             const startFn = function(e) {
-                if (self.$refs.pullRefresh.scrollTop !== 0) return
+
                 startY = e.touches[0].clientY
                 self.doAn = false
             }
 
             const moveFn = function(e) {
+                console.log(self.$refs.pullRefresh, self.$refs.pullRefresh.scrollTop)
+                if (self.$refs.pullRefresh.scrollTop !== 0) return
                 num = e.touches[0].clientY - startY
-
                 if (num > 0 && self.status === 0){
                     self.loading = true
-                    if (self.top < self.headH + margin){
-                        self.top = self.top + speed
+                    if (self.y < self.refreshHeight + margin){
+                        self.y = self.y + speed
                     } else {
-                        self.top = self.headH + margin
+                        self.y = self.refreshHeight + margin
                         self.status = 1
                     }
                 }
@@ -85,7 +95,7 @@ export default {
                 if (Math.abs(num) > 0){
                     self.doAn = true
                     num = 0
-                    if (self.top > self.headH/2){
+                    if (self.y > self.refreshHeight/2){
                         self.start()
                     } else {
                         self.stop()
@@ -107,13 +117,13 @@ export default {
         },
         start(){
             this.status = 2;
-            this.top = this.headH;
+            this.y = this.refreshHeight;
             this.doAn = true
             this.loading = true
         },
         stop(){
             this.status = 3;
-            this.top = 0;
+            this.y = 0;
             this.doAn = true
             this.loading = false
             setTimeout(()=>{
